fcc4336069e204b1c36106e8d264b1d188ebe678
yrkeviin <kevineziquiellima160@gmail.com>
2025-02-04 10:44:27 -0300

    Refactor: Class serie, add plays

--- src/models/Serie.js
+++ src/models/Serie.js
@@ -1,11 +1,15 @@
 const { v4: uuid4 } = require("uuid");
 
 class Serie {
-    constructor(title, genre, classification) {
+    constructor(title, genre, classification, plays = 0) {
         this.id = uuid4();
         this.title = title;
         this.genre = genre;
         this.classification = classification;
+        this.plays = plays;
+    }
+    play() {
+        this.plays += 1;
     }
 }
 
